a83dec16905b4377e1d687d46e075ce2065c6bbe
wcf <1533144693@qq.com>
2019-07-24 10:46:13 +0800

    update

--- design/tty_community.sql
+++ design/tty_community.sql
@@ -2,13 +2,26 @@ create database tty_community;
 alter database tty_community character set utf8mb4;
 create table user(
     _id integer primary key auto_increment,
-    id text not null unique,
-    nickname text not null unique,
+    id varchar(32) not null unique,
+    nickname varchar(32) not null unique,
     token text not null,
     password text not null,
     last_login_ip text not null,
     last_login_time text not null,
     email text not null,
+    log blob not null
+);
+create table user_detail(
+    _id integer primary key auto_increment,
+    id varchar(32) not null unique,
+    portrait text not null,
+    follower text not null,
+    following text not null,
+    personal_signature text not null,
+    account_status text not null,
+    user_group integer not null,
+    exp integer not null default 20,
+    data blob,
     log blob not null
 );
 create table blog(
